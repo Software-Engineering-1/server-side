28db0c4c7b8c578db4edb87c8a22fb37693d40c8
Karthik Radhakrishnan <Karthik Radhakrishnan>
2016-04-15 20:17:57 +0530

    Changed dashboard

--- se_server/employers/views.py
+++ se_server/employers/views.py
@@ -29,6 +29,22 @@ def login(request):
         return HttpResponse(template.render(context,request))   #render the page
 
 from mainapp.models import *
+
+
+def fill_context(user):
+    context={}
+    context['user']=user
+    p=Personal.objects.all().filter(user_details=user)[0]
+    context['Personal']=p
+    context['PersonOrganization']=PersonOrganization.objects.all().filter(person=p)
+    context['Publication']=Publications.objects.all().filter(author=p)
+    context['Project']=Project.objects.all().filter(person=p)
+    context['Education']=Education.objects.all().filter(person=p)
+    context["Skills"]=set(Skill.objects.all())-set(p.skills.all())
+    return context
+
+
+
 @csrf_exempt
 def home(request):
     owner=User.objects.all().filter(username=request.session.get('user'))[0]
@@ -38,14 +54,21 @@ def home(request):
         apps=JobApplication.objects.all().filter(job=job)
         app_contexts=[]
         for app in apps:
+            """
             single_app_context={}
             single_app_context["name"]=app.applicant.user_details.username
             single_app_context["score"]=app.score
             single_app_context["date"]=app.date
             single_app_context["status"]=app.status
+            """
+            single_app_context=[]
+            single_app_context.append(app.date)
+            single_app_context.append(app.status)
+            single_app_context.append(app.score)
+            single_app_context.append(app.applicant.user_details.username)
             app_contexts.append(single_app_context)
-	pos=job.position
-	pos=pos.replace(" ","_")
+        pos=job.position
+        pos=pos.replace(" ","_")
         job_context[pos]=app_contexts
 	#job_context["display_name"]=job.position
     context={'company-name':jobs.owner,'jobpostings':job_context}
@@ -54,4 +77,12 @@ def home(request):
 
 @csrf_exempt
 def showresume(request):
-	return HttpResponse(request.POST["username"],status=200)
+    if(request.method=="POST" and request.POST.get("username",None)):
+        person1=User.objects.all().filter(username=request.POST["username"])[0]
+        context=fill_context(person1)
+        template=loader.get_template('employers/showcandidateprofile.html')
+        return HttpResponse(template.render(context,request))   #render the page
+    elif(request.session.get('user',None)):
+        redirect(login)
+    else:
+        redirect(home)
